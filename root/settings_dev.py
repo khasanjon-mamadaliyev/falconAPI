0da059e803b01b730b09c22daa6cd5d41173aadd
Khasanjon Mamadaliyev <khasanjon.eng@gmail.com>
2023-06-13 13:04:07 +0500

    updated files

--- root/settings_dev.py
+++ root/settings_dev.py
@@ -1,25 +1,25 @@
-from root.settings import *  # noqa
-
-DEBUG = True
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'falcon',
-        'USER': 'postgres',
-        'PASSWORD': 1,
-        'HOST': 'localhost',
-        'PORT': 5432
-    }
-}
-
-
-CACHES = {
-    "default": {
-        "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "",
-        "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient"
-        },
-        "KEY_PREFIX": "falcon"
-    }
-}
+# from root.settings import *  # noqa
+#
+# DEBUG = True
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'falcon',
+#         'USER': 'postgres',
+#         'PASSWORD': 1,
+#         'HOST': 'localhost',
+#         'PORT': 5432
+#     }
+# }
+#
+#
+# CACHES = {
+#     "default": {
+#         "BACKEND": "django_redis.cache.RedisCache",
+#         "LOCATION": "",
+#         "OPTIONS": {
+#             "CLIENT_CLASS": "django_redis.client.DefaultClient"
+#         },
+#         "KEY_PREFIX": "falcon"
+#     }
+# }
